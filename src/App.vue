dd25d2b16dfcf9f690b6e56f37e4233bb9ec0f92
chihao <chihaoyo@gmail.com>
2017-05-24 00:26:28 +0800

    Remove example code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <div class="d-flex justify-content-between">
       <div v-for="channel in DataStore.channels">{{ channel.id }}</div>
     </div>
@@ -28,11 +27,5 @@ export default {
 @import '~common/styles/main';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
