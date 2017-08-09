e903641a39d15bd9b114b6d4055f0827043c8a58
chihaoyo <chihaoyo@gmail.com>
2017-08-09 13:47:38 +0800

    Update style

--- src/components/Polls.vue
+++ src/components/Polls.vue
@@ -1,8 +1,11 @@
 <template>
-<div class="polls">
-  <router-link class="poll" v-for="poll in polls" :key="poll.id" :to="pollPath(poll)">
-    <h3 class="small">{{ poll.name }}</h3>
-  </router-link>
+<div class="polls-wrapper">
+  <h1 class="small">沃草民調</h1>
+  <div class="polls">
+    <router-link class="poll" v-for="poll in polls" :key="poll.id" :to="pollPath(poll)">
+      <h3 class="small">{{ poll.name }}</h3>
+    </router-link>
+  </div>
 </div><!-- FIXME: make component similar to musou-vue-webpack/SeriesMenu -->
 </template>
 
@@ -26,7 +29,17 @@ export default {
 <style lang="scss">
 @import '~common/src/styles/resources';
 
+.polls-wrapper {
+  // this should be the standard style for container
+  max-width: $bp-sm;
+  margin: 1rem auto;
+  @include bp-sm-down {
+    padding: 0 1rem;
+  }
+}
+// this should be the standard style for a list of links
 .polls {
+  margin: 1rem 0;
   > .poll {
     position: relative;
     display: inline-block;
@@ -35,7 +48,10 @@ export default {
     margin: 0 1rem 1rem 0;
     padding: 1rem;
     background: rgba($color-park, 0.25);
-    @include shadow;
+    @include shadow-minimum;
+    &:hover {
+      @include shadow;
+    }
   }
 }
 </style>
