fa241daa88b66cf004c25bfca330a0464ea1a054
chihaoyo <chihaoyo@gmail.com>
2017-08-10 18:23:36 +0800

    Minor update

--- src/data/polls.js
+++ src/data/polls.js
@@ -5,7 +5,7 @@ export default [
     type: 'people',
     name: '桃園立委給問嗎',
     question: '這些桃園的立法委員，你想問誰？',
-    description: '得票數前四名，沃草將會邀請委員參加九月的《桃園立委給問嗎》。',
+    description: '沃草將會邀請得票數前四位的委員參加九月的《桃園立委給問嗎》。',
     options: [
       {
         id: '鄭運鵬',
