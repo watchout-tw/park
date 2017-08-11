f0f682e9445beae4dcc5dd5796a15234a11c6b91
chihaoyo <chihaoyo@gmail.com>
2017-08-12 00:42:00 +0800

    Add ?login to Everyone

--- src/components/Everyone.vue
+++ src/components/Everyone.vue
@@ -4,5 +4,22 @@
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isAuthenticated() {
+      return this.$store.state.isAuthenticated
+    }
+  },
+  mounted() {
+    if(this.$route.query.hasOwnProperty('login')) {
+      if(!this.isAuthenticated) {
+        this.$store.dispatch('toggleModalAuth', {
+          value: true
+        })
+      } else {
+        alert('歡迎回到沃草共有地')
+      }
+    }
+  }
+}
 </script>
