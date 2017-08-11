c994eeb3a819e5a1a9c5d36ed42c99b06cf2bf76
chihaoyo <chihaoyo@gmail.com>
2017-08-12 00:41:47 +0800

    Disable some lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     // allow paren-less functions
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0, // allow paren-less functions
+    'keyword-spacing': 0,
+    'no-new': 0,
   }
 }
