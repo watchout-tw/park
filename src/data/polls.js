5211eac8ff5197fe3efcfdf4a95190107bd81776
chihao <chihaoyo@gmail.com>
2017-08-14 17:59:08 +0800

    Add more into to poll options

--- src/data/polls.js
+++ src/data/polls.js
@@ -15,43 +15,55 @@ export default [
         id: '鄭運鵬',
         name: '鄭運鵬',
         party: '民進黨',
-        district: '桃園市第一選舉區'
+        zone: '桃園市',
+        district: '第一選舉區',
+        neighborhoods: ['蘆竹', '龜山', '桃園']
       },
       {
         id: '陳賴素美',
         name: '陳賴素美',
         party: '民進黨',
-        district: '桃園市第二選舉區'
+        zone: '桃園市',
+        district: '第二選舉區',
+        neighborhoods: ['大園', '觀音', '新屋', '楊梅']
       },
       {
         id: '陳學聖',
         name: '陳學聖',
         party: '國民黨',
-        district: '桃園市第三選舉區'
+        zone: '桃園市',
+        district: '第三選舉區',
+        neighborhoods: ['中壢']
       },
       {
         id: '鄭寶清',
         name: '鄭寶清',
         party: '民進黨',
-        district: '桃園市第四選舉區'
+        zone: '桃園市',
+        district: '第四選舉區',
+        neighborhoods: ['桃園']
       },
       {
         id: '呂玉玲',
         name: '呂玉玲',
         party: '國民黨',
-        district: '桃園市第五選舉區'
+        zone: '桃園市',
+        district: '第五選舉區',
+        neighborhoods: ['平鎮', '龍潭']
       },
       {
         id: '趙正宇',
         name: '趙正宇',
         party: '無黨籍',
-        district: '桃園市第六選舉區'
+        zone: '桃園市',
+        district: '第六選舉區',
+        neighborhoods: ['八德', '大溪', '復興', '中壢']
       },
       {
         id: '吳志揚',
         name: '吳志揚',
         party: '國民黨',
-        district: '全國不分區'
+        zone: '全國不分區'
       }
     ],
     related_event: {
