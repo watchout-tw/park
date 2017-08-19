c226a27828f7484acdd59c412de547d3cad7c4d2
chihaoyo <chihaoyo@gmail.com>
2017-08-19 12:39:43 +0800

    Add ancm

--- src/components/Park.vue
+++ src/components/Park.vue
@@ -2,6 +2,16 @@
 <div class="park">
   <div class="key-visual"><img :src="keyVisual" /></div>
   <polls></polls>
+  <div v-if="ancmIsShown" class="ancm-wrapper panel-wrapper-center">
+    <div class="ancm panel bg-park-light-opaque" @click.stop="goAncm">
+      <div class="panel-heading contain-underline-text"><span>沃草共有地公告</span></div>
+      <div class="marquee">
+        <div class="content">{{ ancmText }}</div>
+      </div>
+      <div class="panel-action"><button class="park soft">我要投票</button></div>
+      <div class="close" @click.self.stop="ancmIsShown = false"></div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -30,7 +40,10 @@ export default {
   },
   data() {
     return {
-      keyVisual: require('_/park.png')
+      keyVisual: require('_/park.png'),
+      ancmIsShown: true,
+      ancmText: '桃園立委給問嗎？票選活動現正進行中。',
+      ancmGoRoute: '/kangsim/ask-taoyuan'
     }
   },
   beforeMount() {
@@ -69,6 +82,9 @@ export default {
   methods: {
     hasQueryParameter(key) {
       return this.$route.query.hasOwnProperty(key)
+    },
+    goAncm() {
+      this.$router.push(this.ancmGoRoute)
     }
   },
   components: {
@@ -78,7 +94,83 @@ export default {
 </script>
 
 <style lang="scss">
+@import '~common/src/styles/resources';
+
+// FIXME: move to common when ready
+@keyframes marquee {
+  0% {
+    transform: translateX(0)
+  }
+  100% {
+    transform: translateX(-100%)
+  }
+}
+.marquee {
+  position: relative;
+  width: 100%;
+  height: 1rem;
+  line-height: 1rem;
+  overflow: hidden;
+
+  > .content {
+    position: absolute;
+    display: block;
+    width: auto;
+    padding-left: 100%;
+    white-space: nowrap;
+    overflow: visible;
+    animation: marquee 8s linear infinite;
+  }
+}
+
+.panel {
+  position: relative;
+  max-width: 16rem;
+  margin: 0 1rem 1rem 0;
+  padding: 1rem;
+  cursor: pointer;
+  @include shadow-minimum;
+  &:hover {
+    @include shadow;
+  }
+
+  > .panel-heading {
+    @include font-serif;
+    font-size: 0.875rem;
+    font-weight: bold;
+    line-height: 1;
+    text-align: center;
+    margin-bottom: 1.5rem;
+  }
+  > .panel-action {
+    margin-top: 1rem;
+    text-align: center;
+  }
+}
+.panel-wrapper-center {
+  > .panel {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+.contain-underline-text {
+  > span {
+    padding-bottom: 2px;
+    border-bottom: 2px black solid;
+  }
+}
+
+// This stays here
 .park {
+  > .ancm-wrapper {
+    position: absolute;
+    top: $navbar-height;
+    left: 0;
+    margin-top: 1rem;
+    width: 100%;
+    z-index: $z-fixed;
+  }
   > .key-visual {
     width: 100%;
     > img {
